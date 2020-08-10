554ec8f00c05c127d2d56e2a4725a05ce22ed702
boringtao <xinghui.tao@wecorpindustries.com>
2020-08-10 15:19:21 +0100

    Add: plot

--- scripts/plot.py
+++ scripts/plot.py
@@ -4,6 +4,7 @@ import gym
 
 from stable_baselines.bench import Monitor
 from stable_baselines.common.callbacks import CallbackList, EvalCallback, StopTrainingOnRewardThreshold
+from stable_baselines.common.cmd_util import make_vec_env
 from stable_baselines.common.evaluation import evaluate_policy
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
@@ -12,20 +13,21 @@ from stable_baselines import A2C, SAC, PPO2, TD3
 import utils.warning_ignore
 from utils import ProgressBarManager
 from utils import PlottingCallback
+plotting_callback = PlottingCallback()
 checkpoints_dir = "checkpoint"
 log_dir = "logs"
 
 env_id = 'Pendulum-v0'
-env = gym.make(env_id)
-env = Monitor(env, log_dir)
-
+# env = gym.make(env_id)
+# env = Monitor(env, log_dir)
+env = make_vec_env(env_id, n_envs=1, monitor_dir=log_dir)
 
 callback_on_best = StopTrainingOnRewardThreshold(reward_threshold=10, verbose=1)
 eval_callback = EvalCallback(env, callback_on_new_best=callback_on_best, verbose=1)
 
 model = PPO2(MlpPolicy, env, verbose=0, tensorboard_log=log_dir)
 with ProgressBarManager(5000) as progress_callback:
-    callback = CallbackList([progress_callback, eval_callback])
+    callback = CallbackList([progress_callback, plotting_callback, eval_callback])
     model.learn(total_timesteps=5000, callback=callback, reset_num_timesteps=False)
 
 model.save(checkpoints_dir + "/PPO2_Pendulum")
--- scripts/utils/plotting.py
+++ scripts/utils/plotting.py
@@ -5,6 +5,8 @@ import numpy as np
 from stable_baselines.common.callbacks import BaseCallback
 from stable_baselines.results_plotter import load_results, ts2xy
 
+log_dir = "logs"
+
 
 class PlottingCallback(BaseCallback):
     """
