726c200b1bbb23c717160775f5746c2fb5bd74b3
boringtao <xinghui.tao@wecorpindustries.com>
2020-08-10 13:17:58 +0100

    Add: callback on best

--- scripts/save.py
+++ scripts/save.py
@@ -2,18 +2,29 @@
 
 import gym
 
+from stable_baselines.bench import Monitor
+from stable_baselines.common.callbacks import EvalCallback, StopTrainingOnRewardThreshold
 from stable_baselines.common.evaluation import evaluate_policy
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import A2C, SAC, PPO2, TD3
 
 import utils.warning_ignore
+checkpoints_dir = "checkpoint"
+log_dir = "logs"
+
+env_id = 'Pendulum-v0'
+env = gym.make(env_id)
+env = Monitor(env, log_dir)
 
 
-checkpoints_dir = "checkpoint" 
-model = PPO2('MlpPolicy', 'Pendulum-v0', verbose=0).learn(8000)
+callback_on_best = StopTrainingOnRewardThreshold(reward_threshold=10, verbose=1)
+eval_callback = EvalCallback(env, callback_on_new_best=callback_on_best, verbose=1)
 
-obs = model.env.observation_space.sample()
-print("pre saved", model.predict(obs, deterministic=True))
+model = PPO2(MlpPolicy, env, verbose=1, tensorboard_log=log_dir)
+model.learn(total_timesteps=5000, callback=eval_callback, reset_num_timesteps=False)
 
 model.save(checkpoints_dir + "/PPO2_Pendulum")
+
+obs = model.env.observation_space.sample()
+print(model.predict(obs, deterministic=True))
